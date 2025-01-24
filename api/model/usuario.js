2484696bcaede3142492a6e0f297502a5af0b2f2
Andre <warder63@gmail.com>
2025-01-24 09:45:59 -0300

    model usuario update2

--- api/model/usuario.js
+++ api/model/usuario.js
@@ -53,3 +53,36 @@ const UsuariosSchema = new mongoose.Schema({
         const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
         return hash === this.hash;
     }
+    UsuariosSchema.methods.gerarToken = function(){
+        const hoje = new Date();
+        const exp = new Date(hoje);
+        exp.setDate(hoje.getDate() + 15);
+
+        return jwt.sign({
+            id: this._id,
+            email: this.email,
+            nome: this.nome,
+            exp: parseInt(exp.getTime() / 1000, 10)
+        }, secret);
+    }
+    UsuariosSchema.methods.enviarAuthJSON = function(){
+        return {
+            _id: this._id,
+            nome: this.nome,
+            email: this.email,
+            loja: this.loja,
+            role: this.permissao,
+            token: this.gerarToken()
+        }
+    }
+    UsuariosSchema.methods.criarTokenRecuperacaoSenha = function(){
+        this.recovery = {};
+        this.recovery.token = crypto.randomBytes(16).toString('hex');
+        this.recovery.date = new Date(new Date().getTime() + 24*60*60*1000);
+        return this.recovery;
+    }
+    UsuariosSchema.methods.finalizarTokenRecuperacaoSenha = function(){
+        this.recovery = {token: null, date: null};
+        return this.recovery;
+    }
+    module.exports = mongoose.model('Usuario', UsuariosSchema);
